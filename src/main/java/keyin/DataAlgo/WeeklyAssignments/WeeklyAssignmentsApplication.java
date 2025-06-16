0ac6ad42ad2d2f4177f74b8d3c1afdeb26868319
Gerogie18 <jennlyver@gmail.com>
2025-06-16 14:21:10 -0230

    Merge Spring boot with code

---
+++ src/main/java/keyin/DataAlgo/WeeklyAssignments/WeeklyAssignmentsApplication.java
@@ -0,0 +1,13 @@
+package keyin.DataAlgo.WeeklyAssignments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyAssignmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyAssignmentsApplication.class, args);
+	}
+
+}

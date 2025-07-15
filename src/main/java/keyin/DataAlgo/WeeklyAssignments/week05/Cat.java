3cb5c5762981d3cab95303c1bf90ab5bf9c615bd
Gerogie18 <jennlyver@gmail.com>
2025-07-15 00:37:54 -0230

    Create Cat and Dog Objects

---
+++ src/main/java/keyin/DataAlgo/WeeklyAssignments/week05/Cat.java
@@ -0,0 +1,12 @@
+package keyin.DataAlgo.WeeklyAssignments.week05;
+
+public class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat: " + name;
+    }
+}
---
+++ src/main/java/keyin/DataAlgo/WeeklyAssignments/week05/Dog.java
@@ -0,0 +1,12 @@
+package keyin.DataAlgo.WeeklyAssignments.week05;
+
+public class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Dog: " + name;
+    }
+}
